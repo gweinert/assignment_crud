425699531caf7ca557df13ccf7ec71530e686fa3
Garrett <garrettweinert@gmail.com>
2015-07-18 12:03:11 -0700

    Fixed how to edit a tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,7 @@
 class TweetsController < ApplicationController
 
   include TweetsHelper
+  
   def create
     @tweet = Tweet.new(tweet_params)
     @tweet.user_id = params[:user_id]
@@ -8,6 +9,30 @@ class TweetsController < ApplicationController
     @tweet.save
 
     redirect_to user_path(@tweet.user)
+  end
+
+  def edit
+    @tweet = Tweet.find(params[:id])
+    @user = User.find(params[:user_id])
+    # @tweet.user_id = params[:user_id]
+    # redirect_to edit_user_tweet_path(@tweet.user_id, @tweet)
+  end
+
+  def update
+    @tweet = Tweet.find(params[:id])
+    @tweet.user_id = params[:user_id]
+    @tweet.update(tweet_params)
+
+    redirect_to user_path(@tweet.user)
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:id])
+    user_id = params[:user_id]
+
+    tweet.destroy
+
+    redirect_to user_path(user_id)
   end
 
 
